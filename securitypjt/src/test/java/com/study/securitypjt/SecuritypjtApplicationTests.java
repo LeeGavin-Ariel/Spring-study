f134cc99fcebc828ccfc5f5ea9132005cd5b9f48
LeeGavin-Ariel <kv829ly@naver.com>
2024-05-28 21:17:39 +0900

    [study-security] securitypjt first init

---
+++ securitypjt/src/test/java/com/study/securitypjt/SecuritypjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.securitypjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritypjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
